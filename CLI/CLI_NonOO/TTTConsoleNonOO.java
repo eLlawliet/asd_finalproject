bb14ab9d7a1e967412e15f179ae81bff71b6d126
iel <herobrinealegend@gmail.com>
2024-11-25 08:13:42 +0700

    Adding Header

--- CLI/CLI_NonOO/TTTConsoleNonOO.java
+++ CLI/CLI_NonOO/TTTConsoleNonOO.java
@@ -1,3 +1,13 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #1
+ * 1 - 5026231010 - Daniel Setiawan Yulius Putra
+ * 2 - 5026231160 - Muhammad Gandhi Taqi Utomo
+ * 3 - 5026231184 - Dzaky Ahmad
+ */
+
 import java.util.Scanner;
 /**
  * Tic-Tac-Toe: Two-player, console-based, non-graphics, non-OO version.
